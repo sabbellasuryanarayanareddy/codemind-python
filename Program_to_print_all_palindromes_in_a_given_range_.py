45e940a790d7f52221596a5071d88a018a51bf42
suryanarayanareddy sabbella <83954595+sabbellasuryanarayanareddy@users.noreply.github.com>
2022-10-26 20:38:52 +0530

    a=int(input()) b=int(input()) for n in range(a,b+1):     t=n     rev=0     while n>0:         r=n%10         rev=rev*10+r         n=n//10     if rev==t:         print(t,end=" ")

---
+++ Program_to_print_all_palindromes_in_a_given_range_.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+for n in range(a,b+1):
+    t=n
+    rev=0
+    while n>0:
+        r=n%10
+        rev=rev*10+r
+        n=n//10
+    if rev==t:
+        print(t,end=" ")
+    
